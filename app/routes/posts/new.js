4c59c432e2f20561102492d4dad3e9df621c1fd4
Lissete <liss.sb@gmail.com>
2015-06-26 13:42:58 +0200

    Imprive post.new

--- app/routes/posts/new.js
+++ app/routes/posts/new.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(){
     return this.store.createRecord('post');
+  },
+  deactivate(){
+    const model = this.get('controller.model');
+    if(model.get('isDirty')){
+        model.destroyRecord();
+    }
   }
 });
